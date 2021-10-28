4b886d5f564c5797cbc254c0c68d81d1798c64f8
Ulysse <89702597+DalasK@users.noreply.github.com>
2021-10-28 14:41:04 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include <tchar.h>
-#include "res.h"
+#include "res.h" // fichier ressource qui contient l'image bmp de logo
 
 static TCHAR szWindowClass[] = _T("DVD-Bouncing-Logo");
 
-// La chaÓne de cractËre qui apparaÓtra dans le titre de l'application
+// La cha√Æne de cract√®re qui appara√Ætra dans le titre de l'application
 static TCHAR szTitle[] = _T("DVD-Bouncing-Logo");
 
 
 DWORD WINAPI DVD_bounce(LPVOID lparam) ;
-RECT rect;  // on verra qu'il sera utile dans la suite du code d'utiliser une structure RECT (pour dÈfinir un rectangle (ma fenÍtre GUI) par les coordonnÈes de ses coins supÈrieur gauche et infÈrieur droit)
+RECT rect;  // on verra qu'il sera utile dans la suite du code d'utiliser une structure RECT (pour d√©finir un rectangle (ma fen√™tre GUI) par les coordonn√©es de ses coins sup√©rieur gauche et inf√©rieur droit)
 
-int w = GetSystemMetrics(SM_CXSCREEN) - 200; // Largeur X de ma fenÍtre
-int h = GetSystemMetrics(SM_CYSCREEN) - 200; // Hauteur Y de ma fenÍtre
+int w = GetSystemMetrics(SM_CXSCREEN) - 200; // Largeur X de ma fen√™tre
+int h = GetSystemMetrics(SM_CYSCREEN) - 200; // Hauteur Y de ma fen√™tre
 		
-LRESULT CALLBACK WndProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam) // fonction qu'on rappellera dans WinMain (il s'agit du background de la fenÍtre) : 
+LRESULT CALLBACK WndProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam) // fonction qu'on rappellera dans WinMain (il s'agit du background de la fen√™tre) : 
 {
 	switch (Msg)
 	{
@@ -24,7 +24,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam) // f
 		
 		PAINTSTRUCT ps;
 		
-		HDC hdc = BeginPaint(hWnd, &ps); // contexte spÈcifiÈ dans lequel la couleur sera appliquÈe
+		HDC hdc = BeginPaint(hWnd, &ps); // contexte sp√©cifi√© dans lequel la couleur sera appliqu√©e
 		
 		
 		HBRUSH noir = CreateSolidBrush(RGB(0, 0, 0)); // couleur noire
@@ -54,7 +54,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam) // f
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	// les principaux paramËtres de notre fenÍtre (flemme de dÈcrire) 
+	// les principaux param√®tres de notre fen√™tre (flemme de d√©crire) 
      WNDCLASSEXA c;
 	c.cbSize = sizeof(WNDCLASSEX);
 	c.lpfnWndProc = WndProc;
@@ -76,12 +76,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	}
 
 	HWND DVDWin = CreateWindowEx(WS_EX_OVERLAPPEDWINDOW , szWindowClass, szTitle, WS_OVERLAPPED | WS_MINIMIZEBOX | WS_SYSMENU, 0, 0, w, h, NULL, NULL, hInstance, NULL);
-	// crÈation d'une fenÍtre noire, avec l'affichage du titre et du logo, impossible ‡ redimensionner gr‚ce aux attributs (WS_OVERLAPPED | WS_MINIMIZEBOX | WS_SYSMENU)
+	// cr√©ation d'une fen√™tre noire, avec l'affichage du titre et du logo, impossible √† redimensionner gr√¢ce aux attributs (WS_OVERLAPPED | WS_MINIMIZEBOX | WS_SYSMENU)
 	
-	ShowWindow(DVDWin, nCmdShow); // affichage de la fenÍtre 
-	UpdateWindow(DVDWin); // puis on la met ‡ jour
+	ShowWindow(DVDWin, nCmdShow); // affichage de la fen√™tre 
+	UpdateWindow(DVDWin); // puis on la met √† jour
 
-    CreateThread(0, 0, &DVD_bounce, 0, 0, 0); // on appel notre fonction sous la forme d'un thread pour Èviter de bloquer le programme (avec la boucle infinie) 
+    CreateThread(0, 0, &DVD_bounce, 0, 0, 0); // on appel notre fonction sous la forme d'un thread pour √©viter de bloquer le programme (avec la boucle infinie) 
 
 	MSG msg;
 
@@ -95,18 +95,18 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 DWORD WINAPI DVD_bounce(LPVOID lparam) 
 {
 	HBITMAP monDVD = (HBITMAP)LoadImage(GetModuleHandle(0), MAKEINTRESOURCE(IDB_IMAGE), IMAGE_BITMAP, 0, 0, LR_DEFAULTSIZE | LR_CREATEDIBSECTION | LR_LOADMAP3DCOLORS); // on charge notre image depuis un fichier ressource (le fameux logo DVD) 
-    HWND DVDWin = FindWindowW(L"DVD-Bouncing-Logo", L"DVD-Bouncing-Logo"); // on rÈcupËre le handle de notre fenÍtre (une fois sa crÈation et son affichage)
+    HWND DVDWin = FindWindowW(L"DVD-Bouncing-Logo", L"DVD-Bouncing-Logo"); // on r√©cup√®re le handle de notre fen√™tre (une fois sa cr√©ation et son affichage)
     
 	HDC hdc = GetDC(DVDWin); 
-    HDC hdc_memoire = CreateCompatibleDC(hdc); // rÈcupËre le handle prÈcÈdent (stock en mÈmoire) compatible 
+    HDC hdc_memoire = CreateCompatibleDC(hdc); // r√©cup√®re le handle pr√©c√©dent (stock en m√©moire) compatible 
     
-    SelectObject(hdc_memoire, monDVD); // selection du contexte spÈcifiÈ et de notre image 
-    GetWindowRect(DVDWin, &rect); // rÈcupËre les dimensions du rectangle englobant de la fenÍtre spÈcifiÈe
+    SelectObject(hdc_memoire, monDVD); // selection du contexte sp√©cifi√© et de notre image 
+    GetWindowRect(DVDWin, &rect); // r√©cup√®re les dimensions du rectangle englobant de la fen√™tre sp√©cifi√©e
     
 	int srcw = rect.right; // largeur source 
 	int srch = rect.bottom; // hauteur source 
 	
-    int mouvementX = 5; int mouvementY = 5; // la vitesse ‡ laquelle va se dÈplacer le logo
+    int mouvementX = 5; int mouvementY = 5; // la vitesse √† laquelle va se d√©placer le logo
 	
 	
 		while(true) {
@@ -115,24 +115,24 @@ DWORD WINAPI DVD_bounce(LPVOID lparam)
 	          int yposition = rect.top += mouvementY; 
 	
 
-			BitBlt(hdc, xposition, yposition, srcw, srch, hdc_memoire, 0, 0, SRCCOPY); // on dessine notre image (qui se dÈplacera en fonction des coordonnÈes XY
+			BitBlt(hdc, xposition, yposition, srcw, srch, hdc_memoire, 0, 0, SRCCOPY); // on dessine notre image (qui se d√©placera en fonction des coordonn√©es XY
 
-			if (yposition >= srch - 200) { // si la position Y dÈpasse la valeur correspondant ‡ la hauteur de la fenÍtre (dÈfinit plus haut) alors: 
-				mouvementY = -mouvementY; // mouvement nÈgatif donc direction changÈe
+			if (yposition >= srch - 200) { // si la position Y d√©passe la valeur correspondant √† la hauteur de la fen√™tre (d√©finit plus haut) alors: 
+				mouvementY = -mouvementY; // mouvement n√©gatif donc direction chang√©e
 			}
 
-			if (xposition >= srcw - 300) { // si la position X dÈpasse la valeur correspondant ‡ la largeur de la fenÍtre (dÈfinit plus haut) alors: 
+			if (xposition >= srcw - 300) { // si la position X d√©passe la valeur correspondant √† la largeur de la fen√™tre (d√©finit plus haut) alors: 
 				mouvementX = -mouvementX; // Idem
 
 			}
-			if (yposition <= 0) { //mÍme chose mais position Y infÈrieure ‡ 0
+			if (yposition <= 0) { //m√™me chose mais position Y inf√©rieure √† 0
 				mouvementY = -mouvementY;
 			}
 
-			if (xposition <= 0) { //mÍme chose mais valeur X infÈrieure ‡ 0 
+			if (xposition <= 0) { //m√™me chose mais valeur X inf√©rieure √† 0 
 				mouvementX = -mouvementX;
 			}
-			// crÈant ainsi un rebondissement sur les bords de la fenÍtre infini
+			// cr√©ant ainsi un rebondissement sur les bords de la fen√™tre infini
 			Sleep(10); // pour avoir un mouvement stable 
 }
 }
